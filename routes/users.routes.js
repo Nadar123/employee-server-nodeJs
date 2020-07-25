476b8ed4549d72c7ac2319e066e3ecc82e35397c
nadar123 <nadar.rosano@gmail.com>
2020-07-25 11:08:57 +0300

    validation and routes

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator/check');
+
+const User = require('../models/User');
 
 // @route post api/users
-// @desc regiter a user
+// @desc regsiter a user
 // @access public
-router.post('/', (req, res) => {
-  res.send('register a user')
+router.post('/', [
+  body('name', 'Please add your name').not().isEmpty(),
+  body('password', 'Please enter a pwassword with more then 6 characters').isLength({ min: 5 }),
+  body('email', 'Please enter a pwassword with more then 6 characters').isEmail().normalizeEmail()
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  res.send('passed')
+  // User.create({
+  //   username: req.body.username,
+  //   password: req.body.password
+  // }).then(user => res.json(user));
 });
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// DB connection
+connectDB();
+
+// Middleware
+app.use(express.json({extend: false}));
 
 app.get('/', (req, res) => res.json({ msg: 'employees list API'}))
 
 // define routes
-app.use('/api/user', require('./routes/users.routes'));
+app.use('/api/users', require('./routes/users.routes'));
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/employees', require('./routes/employees.routes'));
 
